ab5f6059328e32d58365e3e194cf61f066948d25
Jake McLeman <jake@mcleman.com>
2019-10-04 22:20:44 -0700

    Added health and projectile script

---
+++ LD45/Assets/Scripts/Health.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth;
+    public float damageMultiplier;
+
+    private float curHealth;
+
+    public float CurrentHealth 
+    {
+        get { return curHealth; }
+    }
+
+    public float HealthRatio 
+    {
+        get { return curHealth / maxHealth; }
+    }
+
+    private void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    /*
+     * Deal an amount of damage, returns true if this deals a killing blow, false otherwise
+     */
+    public bool TakeDamage(float amount) 
+    {
+        if (Debug.isDebugBuild) 
+        {
+            if(amount < 0) Debug.LogWarningFormat("Requested negative damage {0} to be dealt to {1}}", amount, gameObject.name);
+        }
+
+        curHealth -= amount * damageMultiplier;
+
+        if(curHealth < 0)
+        {
+            // TODO: die
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    /*
+     * Heal an amount of damage, returns a boolean of if any healing was able to be done
+     * (false if no healing was requested, or if health is already max)
+     */
+    public bool Heal(float amount) 
+    {
+        // Check if healing is possible to avoid wasting health packs
+        if(amount == 0 || curHealth >= maxHealth) return false;
+
+        if (Debug.isDebugBuild) 
+        {
+            if(amount < 0) Debug.LogWarningFormat("Requested negative healing {0} to be done to {1}}", amount, gameObject.name);
+        }
+
+        curHealth += amount;
+
+        if (curHealth > maxHealth) curHealth = maxHealth;
+
+        return true;
+    }
+}
---
+++ LD45/Assets/Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // The weapon that launched this projectile 
+    // (so it can be informed about what happened to it if required)
+    public IWeapon shooter;
+
+    // How much hurt
+    public float damageAmount;
+
+    // How fast go
+    public float speed;
+
+    // Normalized direction this projectile should move in
+    public Vector3 direction;
+
+    private void Update()
+    {
+        transform.forward = direction;
+        transform.position += (speed * Time.deltaTime) * direction;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health otherHealth = collision.gameObject.GetComponent<Health>();
+
+        if(otherHealth)
+        {
+            otherHealth.TakeDamage(damageAmount);
+        }
+
+        Destroy(gameObject);
+    }
+}
