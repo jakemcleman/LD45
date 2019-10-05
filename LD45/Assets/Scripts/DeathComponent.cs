a7a1459524748de509230335f1e7bc5e70af4c5e
Jake McLeman <jake@mcleman.com>
2019-10-05 00:09:38 -0700

    Added death

--- LD45/Assets/Scripts/DeathComponent.cs
+++ LD45/Assets/Scripts/DeathComponent.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 [RequireComponent(typeof(Health))]
 public class DeathComponent : MonoBehaviour
 {
+    /*
+     *  Set of game objects that should all be spawned when this entity dies
+     */
+    public GameObject[] spawnOnDeath;
     void Start()
     {
         Health healthComp = GetComponent<Health>();
+
+        healthComp.onDeath.AddListener(OnDeath);
+    }
+
+    private void OnDeath() 
+    {
+        foreach(GameObject toSpawn in spawnOnDeath) 
+        {
+            GameObject spawned = GameObject.Instantiate(toSpawn);
+            spawned.transform.position = transform.position;
+        }
+
+        Destroy(gameObject);
     }
 
 }
