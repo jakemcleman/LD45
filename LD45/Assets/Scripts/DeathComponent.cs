981c07d07fa4071b90998d0b56c2f8e1d375cd69
Jake McLeman <jake@mcleman.com>
2019-10-05 00:02:57 -0700

    Prevented immediately becoming grounded after jump

---
+++ LD45/Assets/Scripts/DeathComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+public class DeathComponent : MonoBehaviour
+{
+    void Start()
+    {
+        Health healthComp = GetComponent<Health>();
+    }
+
+}
