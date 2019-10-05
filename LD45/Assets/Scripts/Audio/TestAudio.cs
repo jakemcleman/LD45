36274f9772b46b6c5b058d61e074aa9915a1a970
Thomas Parrish <thomasrparrish@gmail.com>
2019-10-04 19:00:12 -0700

    FMOD test scene

---
+++ LD45/Assets/Scripts/Audio/TestAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAudio : MonoBehaviour
+{
+    public string TestEventName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+            FMODUnity.RuntimeManager.PlayOneShot(TestEventName, this.transform.position);
+    }
+}
