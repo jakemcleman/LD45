b8cfe862a837aa942792434eda2ef9cc55716581
Jake McLeman <jake@mcleman.com>
2019-10-06 00:38:34 -0700

    Added spinney propellers

---
+++ LD45/Assets/Scripts/PropellerSpin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    public float spinRate = 360;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, spinRate * Time.deltaTime);
+    }
+}
