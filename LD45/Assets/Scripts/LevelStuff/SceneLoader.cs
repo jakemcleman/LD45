0c6fb3878d12c2890044f8749274d54acc17d9dd
Eli Wulff <elijw22@gmail.com>
2019-10-06 01:48:10 -0400

    Working on Scene loading

---
+++ LD45/Assets/Scripts/LevelStuff/SceneLoader.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject canvas;
+
+    public string nextScene;
+    private Scene curScene;
+
+    private bool isLoaded;
+
+    private IEnumerator coroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        canvas = GameObject.FindGameObjectWithTag("UI");
+
+        isLoaded = false;
+    }
+
+    public void StartLoad(bool doUnload)
+    {      
+        if (!isLoaded)
+        {
+            coroutine = LoadSceneAsync(doUnload);
+            StartCoroutine(coroutine);
+        }
+        else
+        {
+            Debug.LogError("New scene has already been loaded");
+        }
+    }
+
+    public void UnLoadScene()
+    {
+        if (isLoaded)
+        {
+            SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByName(nextScene));
+            SceneManager.MoveGameObjectToScene(canvas, SceneManager.GetSceneByName(nextScene));
+            SceneManager.UnloadSceneAsync(curScene);
+        }
+        else
+        {
+            Debug.LogError("New scene is not yet loaded, don't unload the scene till a new scene is loaded :(");
+        }
+    }
+
+    IEnumerator LoadSceneAsync(bool doUnload)
+    {
+        curScene = SceneManager.GetActiveScene();
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        isLoaded = true;
+    }
+}
