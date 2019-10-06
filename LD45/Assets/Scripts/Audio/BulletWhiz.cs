509a4dd82854b4a507ff0a8a87342104aefa9965
Thomas Parrish <thomasrparrish@gmail.com>
2019-10-05 18:10:26 -0700

    Bullet whiz adjustments

--- LD45/Assets/Scripts/Audio/BulletWhiz.cs
+++ LD45/Assets/Scripts/Audio/BulletWhiz.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class BulletWhiz : MonoBehaviour
 {
-    FMOD.Studio.EventInstance BulletWhiz_Event;
-    FMOD.ATTRIBUTES_3D att;
+    private string event_path = "event:/SFX/Weapons/Bullet_Passby";
 
     // Start is called before the first frame update
     void Start()
     {
-        BulletWhiz_Event = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Weapons/Bullet_Passby");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -26,8 +25,6 @@ public class BulletWhiz : MonoBehaviour
 
         Debug.Log("BULLET PASSED BY PLAYER!");
 
-        att = FMODUnity.RuntimeUtils.To3DAttributes(other.gameObject);
-        BulletWhiz_Event.set3DAttributes(att);
-        BulletWhiz_Event.start();
+        FMODUnity.RuntimeManager.PlayOneShot(event_path, other.gameObject.transform.position);
     }
 }
