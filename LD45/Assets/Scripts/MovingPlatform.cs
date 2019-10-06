1e6f475f41309a7964d6621da4850029ccbbd2aa
Alan Tsang <alantsang.work@gmail.com>
2019-10-05 18:28:48 -0700

    Added moving platform prefab.

---
+++ LD45/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    Transform initialLocation;
+    Vector3 initialPos;
+
+    [SerializeField]
+    Transform endLocation;
+    Vector3 endPos;
+
+    [SerializeField]
+    AnimationCurve t_curve;
+
+    bool returning = false;
+
+    [SerializeField]
+    float oneWayTime;
+    float timer;
+
+    private Vector3 vel;
+    public Vector3 velocity
+    {
+        get => vel;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = initialLocation.position;
+        endPos = endLocation.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > oneWayTime)
+        {
+            timer = 0;
+            returning = !returning;
+        }
+        float t = timer / oneWayTime;
+        t = t_curve.Evaluate(t);
+        Vector3 prevPos = transform.position;
+        if (!returning)
+        {
+            transform.position = Vector3.Lerp(initialPos, endPos, t);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(endPos, initialPos, t);
+        }
+        vel = (transform.position - prevPos) / Time.deltaTime;
+    }
+}
