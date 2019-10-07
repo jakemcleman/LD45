6f29cef39fd7f35cb104d9141e526fa81f61335e
Thomas Parrish <thomasrparrish@gmail.com>
2019-10-07 02:56:56 -0700

    Button sounds

---
+++ LD45/Assets/Scripts/Audio/ButtonSounds.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSounds : MonoBehaviour
+{
+    public void ButtonClickSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/UI/UI_ButtonClick", transform.position);
+    }
+}
