58b32c0f11ee93edbae41a384db6b0d9a5b67dec
Eli Wulff <elijw22@gmail.com>
2019-10-07 02:59:35 -0400

    Woo

--- LD45/Assets/Scripts/LevelStuff/ObjectPoofer.cs
+++ LD45/Assets/Scripts/LevelStuff/ObjectPoofer.cs
@@ -43,7 +43,7 @@ public class ObjectPoofer : MonoBehaviour
             animationTime += Time.deltaTime;
             float percent = Mathf.Clamp01(animationTime / poofDuration);
 
-            transform.position = Vector3.LerpUnclamped(poofFromVector, endPosition, moveCurve.Evaluate(percent));
+            transform.localPosition = Vector3.LerpUnclamped(poofFromVector, endPosition, moveCurve.Evaluate(percent));
 
             yield return null;
         }
@@ -68,7 +68,7 @@ public class ObjectPoofer : MonoBehaviour
             animationTime += Time.deltaTime;
             float percent = 1 - Mathf.Clamp01(animationTime / poofDuration);
 
-            transform.position = Vector3.LerpUnclamped(poofFromVector, endPosition, moveCurve.Evaluate(percent));
+            transform.localPosition = Vector3.LerpUnclamped(poofFromVector, endPosition, moveCurve.Evaluate(percent));
 
             yield return null;
         }
