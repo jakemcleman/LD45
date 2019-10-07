40b938056f268464ad51a2df1cc9112915bdb781
Thomas Parrish <thomasrparrish@gmail.com>
2019-10-06 17:33:45 -0700

    Messing with running audio

--- LD45/Assets/Scripts/Audio/PlayerAudio.cs
+++ LD45/Assets/Scripts/Audio/PlayerAudio.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class PlayerAudio : MonoBehaviour
 {
-    private float footstepTime;
-    private float footstepTimeInterval = 0.4f;
+    private float groundFootstepTime;
+    private float wallrunFootstepTime;
+    private float groundFootstepTimeInterval = 0.4f;
+    private float wallrunFootstepTimeInterval;
 
     private MovementController movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        footstepTime = 0.4f;
         movement = GetComponent<MovementController>();
-        movement.onMotionStateEvent.AddListener(Jump);
+        movement.onMotionStateEvent.AddListener(MotionStateChange);
+
+        wallrunFootstepTimeInterval = movement.wallRunBaseSpeed / 100.0f;
+
+        groundFootstepTime = groundFootstepTimeInterval;
+        wallrunFootstepTime = wallrunFootstepTimeInterval;
     }
 
     // Update is called once per frame
@@ -27,20 +33,44 @@ public class PlayerAudio : MonoBehaviour
         switch (movement.CurrentMotionState)
         {
             case MotionState.Running:
-                if (footstepTime > 0)
-                    footstepTime -= Time.deltaTime;
+                if (groundFootstepTime > 0)
+                    groundFootstepTime -= Time.deltaTime;
                 else
                 {
-                    FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Player/Player_Run", this.transform.position);
-                    footstepTime = footstepTimeInterval;
+                    PlayFootstep();
+                    groundFootstepTime = groundFootstepTimeInterval;
+                }
+                break;
+            case MotionState.Wallrun:
+                if (wallrunFootstepTime > 0)
+                    wallrunFootstepTime -= Time.deltaTime;
+                else
+                {
+                    PlayFootstep();
+                    wallrunFootstepTime = wallrunFootstepTimeInterval;
                 }
                 break;
         }
     }
 
-    private void Jump(MotionStateEvent eMotion)
+    private void MotionStateChange(MotionStateEvent eMotion)
     {
-        if (eMotion.nextState == MotionState.Jump)
-            FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Player/Player_Jump", this.transform.position);
+        switch (eMotion.nextState)
+        {
+            case MotionState.JumpStart:
+                PlayJump();
+                break;
+        }
+            
+    }
+
+    private void PlayJump()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Player/Player_Jump", transform.position);
+    }
+
+    private void PlayFootstep()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Player/Player_Run", transform.position);
     }
 }
