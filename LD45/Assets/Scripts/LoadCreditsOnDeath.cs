c5d23a8dcc8ef0fc053ed999bfe28d4cc6eafa60
Jake McLeman <jake@mcleman.com>
2019-10-07 14:51:57 -0700

    Added roll credits

---
+++ LD45/Assets/Scripts/LoadCreditsOnDeath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+[RequireComponent(typeof(Health))]
+public class LoadCreditsOnDeath : MonoBehaviour
+{
+    void Start()
+    {
+        Health healthComp = GetComponent<Health>();
+
+        healthComp.onDeath.AddListener(OnDeath);
+    }
+
+    private void OnDeath() 
+    {
+        SceneManager.LoadScene("EndScreen");
+    }
+}
